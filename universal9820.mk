1d9c1989e33bb1c1154881c4f3c4c7932b78ec37
Andreas Schneider <asn@cryptomilk.org>
2020-02-24 09:45:49 +0100

    Add initial platform files

---
+++ universal9820.mk
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2020 The LineAgeOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PLATFORM_PATH := device/samsung/universal9820-common
+
+DEVICE_PACKAGE_OVERLAYS += \
+    $(PLATFORM_PATH)/overlay
+
+### RECOVERY
+ifeq ($(WITH_TWRP),true)
+# Add Timezone database
+PRODUCT_COPY_FILES += \
+    system/timezone/output_data/iana/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
+
+include $(PLATFORM_PATH)/platform/*.mk
+
+include $(PLATFORM_PATH)/system_prop.mk
+include $(PLATFORM_PATH)/vendor_prop.mk
+endif # WITH_TWRP
