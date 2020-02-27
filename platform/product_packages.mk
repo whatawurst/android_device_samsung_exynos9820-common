7fc2326a488303b40cbab5e7f220419c4dc2d139
Andreas Schneider <asn@cryptomilk.org>
2020-02-27 21:55:10 +0100

    platform: Add gatekeeper service

--- platform/product_packages.mk
+++ platform/product_packages.mk
@@ -1,3 +1,8 @@
+### GATEKEEPER
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service
+
 ### KEYMASTER
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.0-service.samsung
