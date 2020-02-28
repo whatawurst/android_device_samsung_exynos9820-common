9cf63094a9425bf6ef8c07c6c959e6424c57add6
Andreas Schneider <asn@cryptomilk.org>
2020-02-28 17:46:40 +0100

    recovery: Fix init file installation

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := init.recovery.exynos9820
 LOCAL_SRC_FILES := init.recovery.exynos9820.rc
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_STEM := init.recovery.$(TARGET_DEVICE)
+LOCAL_MODULE_STEM := init.recovery.exynos9820
 LOCAL_MODULE_SUFFIX := .rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
