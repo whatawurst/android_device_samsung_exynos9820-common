8c0b2b26a25227de222cc490731075ab1ba64598
Andreas Schneider <asn@cryptomilk.org>
2020-02-29 12:26:27 +0100

    board: Add manifest and build default services

--- platform/product_packages.mk
+++ platform/product_packages.mk
@@ -1,8 +1,83 @@
+### AUDIO
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio@5.0-impl \
+    android.hardware.audio@2.0-service
+
+PRODUCT_PACKAGES += \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@5.0-impl
+
+### BLUETOOTH
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl
+
+### CAMERA
+PRODUCT_PACKAGES += \
+    camera.device@1.0-impl \
+    camera.device@3.2-impl \
+    camera.device@3.3-impl \
+    camera.device@3.4-impl \
+    camera.device@3.5-impl
+
+### DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.2-service.clearkey
+
 ### GATEKEEPER
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
 
+### GRAPHICS
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@3.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
+
 ### KEYMASTER
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.0-service.samsung
+
+### MEDIA
+PRODUCT_PACKAGES += \
+    android.hardware.media.omx@1.0-impl \
+    android.hardware.media.omx@1.0-service
+
+### MEMTRACK
+PRODUCT_PACKAGES += \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service
+
+### NEURALNETWORKS
+PRODUCT_PACKAGES += \
+    android.hardware.neuralnetworks@1.0 \
+    android.hardware.neuralnetworks@1.1 \
+    android.hardware.neuralnetworks@1.2
+
+### RENDERSCRIPT
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
+### SENSORS
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service
+
+### THERMAL
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
+
+### WIFI
+PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.0-service \
+    hostapd \
+    wpa_supplicant \
+    wpa_supplicant.conf
