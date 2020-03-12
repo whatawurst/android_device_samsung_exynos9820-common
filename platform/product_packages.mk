b77173f2e256ca816fa2edfb3018243087c6179f
Andreas Schneider <asn@cryptomilk.org>
2020-03-12 21:27:17 +0100

    platform: Add OMX
    Change-Id: Ic9901c835a0f568d9aa5afc2264ea4fa217ed423

--- platform/product_packages.mk
+++ platform/product_packages.mk
@@ -86,6 +86,20 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libtextclassifier_hash.vendor
 
+### OMX
+PRODUCT_PACKAGES += \
+    libOmxAacEnc \
+    libOmxAmrEnc \
+    libOmxCore \
+    libOmxEvrcEnc \
+    libOmxQcelp13Enc \
+    libOmxVdec \
+    libOmxVdecHevc \
+    libOmxVenc \
+    libc2dcolorconvert \
+    libmm-omxcore \
+    libstagefrighthw
+
 ### POWER
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service.samsung
