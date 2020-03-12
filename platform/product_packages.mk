07e5981ee78a3494854e32fe89256d99b7b93baa
Andreas Schneider <asn@cryptomilk.org>
2020-03-12 17:06:48 +0100

    platform: Add missing audio libs
    Change-Id: I343d7bbf98bba788e93909b671009751c88a9577

--- platform/product_packages.mk
+++ platform/product_packages.mk
@@ -5,6 +5,16 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service
 
 PRODUCT_PACKAGES += \
+    libaudioroute \
+    libtinyalsa \
+    tinymix
+
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    audio.r_submix.default \
+    audio.usb.default
+
+PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@5.0-impl
 
