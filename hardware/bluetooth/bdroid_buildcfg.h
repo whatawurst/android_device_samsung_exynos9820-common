57404a323ffdc1ef43e3c093d884fabd96a22c8a
Andreas Schneider <asn@cryptomilk.org>
2020-03-14 12:15:58 +0100

    bluetooth: Add bdroid build configuration
    Change-Id: Icee283bfe15bd9b1a6772b108e0dff67e0e2ab87

---
+++ hardware/bluetooth/bdroid_buildcfg.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2020 LineageOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _BDROID_BUILDCFG_H
+#define _BDROID_BUILDCFG_H
+
+#ifndef _BDROID_BUILDCFG_H
+#define _BDROID_BUILDCFG_H
+
+#pragma push_macro("PROPERTY_VALUE_MAX")
+
+#if !defined(OS_GENERIC)
+#include <cutils/properties.h>
+#include <string.h>
+
+static inline const char* getBTDefaultName()
+{
+    char device[PROPERTY_VALUE_MAX];
+    property_get("ro.boot.hardware", device, "");
+
+    if (!strcmp("beyond0lte", device)) {
+        return "Galaxy S10e";
+    }
+
+    if (!strcmp("beyond2lte", device)) {
+        return "Galaxy S10+";
+    }
+
+    return "Galaxy S10";
+}
+
+#define BTM_DEF_LOCAL_NAME getBTDefaultName()
+#endif /* OS_GENERIC */
+
+/* Wide-band speech support */
+#define BTM_WBS_INCLUDED TRUE
+#define BTIF_HF_WBS_PREFERRED TRUE
+
+/* Vendor extensions */
+#define BLE_VND_INCLUDED TRUE
+
+#pragma pop_macro("PROPERTY_VALUE_MAX")
+#endif
+
+#endif /* _BDROID_BUILDCFG_H */
