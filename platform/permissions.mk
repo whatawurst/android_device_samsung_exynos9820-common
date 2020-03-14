254c41791454370d2d194ab9249fdd77b5507498
Andreas Schneider <asn@cryptomilk.org>
2020-03-14 11:40:15 +0100

    permissions: Add fdroid
    Change-Id: Ib88a0d1ade80df0547fe7248d1ce6b61d4ab32df

--- platform/permissions.mk
+++ platform/permissions.mk
@@ -53,3 +53,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
+
+PRODUCT_COPY_FILES += \
+    $(PLATFORM_PATH)/config/permissions/privapp-permissions-fdroid.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-fdroid.xml
