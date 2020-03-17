68ef02dcac66e1ff6642d13d93f4a2f893c0d361
derfelot <5124143+derfelot@users.noreply.github.com>
2020-03-17 20:25:23 +0100

    platform: Build camera legacy libs

--- platform/product_packages.mk
+++ platform/product_packages.mk
@@ -32,6 +32,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4 \
     android.hardware.camera.provider@2.5 \
+    android.hardware.camera.provider@2.4-legacy \
+    android.hardware.camera.provider@2.5-legacy \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     camera.device@3.3-impl \
