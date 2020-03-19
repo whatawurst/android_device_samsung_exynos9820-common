a2c056bc0a3cbc843b574434728239652e67d1b9
Andreas Schneider <asn@cryptomilk.org>
2020-03-19 22:54:52 +0100

    platform: Fix typo
    Change-Id: Ib5c1397f05d3d4fa22173e0ed1e0dd138e63f751

--- platform/config.mk
+++ platform/config.mk
@@ -4,4 +4,4 @@ PRODUCT_COPY_FILES += \
 
 ### KEYLAYOUT
 PRODUCT_COPY_FILES += \
-    $(PLATFORM_PATH)config/keylayout/gpio_keys.kl:$(TARGET_COPY_OUT)/usr/keylayout/gpio_keys.kl
+    $(PLATFORM_PATH)/config/keylayout/gpio_keys.kl:$(TARGET_COPY_OUT)/usr/keylayout/gpio_keys.kl
