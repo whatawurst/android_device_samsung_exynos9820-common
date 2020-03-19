83a9e3948c3c977cc6986e07db6571b2246fb847
Andreas Schneider <asn@cryptomilk.org>
2020-03-19 21:09:29 +0100

    config: Add keylayout for gpio_keys
    Change-Id: I8ebc38f9b1e5a481edd609b3a9ca9bdad5668e49

--- platform/config.mk
+++ platform/config.mk
@@ -1,3 +1,7 @@
 ### GPS
 PRODUCT_COPY_FILES += \
     $(PLATFORM_PATH)/config/gps/gps_psds.conf:$(TARGET_COPY_OUT)/etc/gps/gps_psds.conf
+
+### KEYLAYOUT
+PRODUCT_COPY_FILES += \
+    $(PLATFORM_PATH)config/keylayout/gpio_keys.kl:$(TARGET_COPY_OUT)/usr/keylayout/gpio_keys.kl
