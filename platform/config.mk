ae9050b048ed8b15c4072438dca99a71292f1c1c
Andreas Schneider <asn@cryptomilk.org>
2020-03-20 08:09:52 +0100

    platform: Add audio policy configuration
    This copies it to the right location and uses abolute paths to other
    config files.
    
    Change-Id: I056b7b2a799db0a36320c9c64a505465fd9eff05

--- platform/config.mk
+++ platform/config.mk
@@ -1,3 +1,8 @@
+### AUDIO
+PRODUCT_COPY_FILES += \
+    $(PLATFORM_PATH)/config/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_configuration.xml \
+    $(PLATFORM_PATH)/config/audio/audio_policy_configuration_sec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_configuration_sec.xml
+
 ### GPS
 PRODUCT_COPY_FILES += \
     $(PLATFORM_PATH)/config/gps/gps_psds.conf:$(TARGET_COPY_OUT)/etc/gps/gps_psds.conf
