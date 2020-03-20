3e75afe00ebd006e85c93b97dfd209d339a067a0
Andreas Schneider <asn@cryptomilk.org>
2020-03-20 08:21:46 +0100

    platform: Fix bluetooth packages
    Change-Id: Iafe375ccdda98bbe753c9a315f2e713edc00810f

--- platform/product_packages.mk
+++ platform/product_packages.mk
@@ -19,14 +19,16 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@5.0-impl
 
-PRODUCT_PACKAGES += \
-    audio.bluetooth.default \
-    android.hardware.bluetooth.audio@2.0-impl
-
 ### BLUETOOTH
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
     android.hardware.bluetooth@1.0-service
+
+PRODUCT_PACKAGES += \
+    audio.bluetooth.default \
+    android.hardware.bluetooth.a2dp@1.0 \
+    android.hardware.bluetooth.audio@2.0 \
+    android.hardware.bluetooth.audio@2.0-impl
 
 ### CAMERA
 PRODUCT_PACKAGES += \
