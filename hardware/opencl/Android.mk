9d94490de6f05364d550f200b5be3e47266bc342
derfelot <5124143+derfelot@users.noreply.github.com>
2020-03-26 15:26:33 +0100

    hardware: Generate all remaining symlinks to libGLES_mali.so

--- hardware/opencl/Android.mk
+++ hardware/opencl/Android.mk
@@ -2,13 +2,49 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LIBGLES_MALI_LIBRARY := /vendor/lib64/egl/libGLES_mali.so
+LIBGLES_MALI_LIBRARY := /vendor/lib/egl/libGLES_mali.so
+LIBGLES_MALI64_LIBRARY := /vendor/lib64/egl/libGLES_mali.so
 
-LIBOPENCL_SYMLINK := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/libOpenCL.so
+LIBOPENCL_SYMLINK := $(TARGET_OUT_VENDOR)/lib/libOpenCL.so
 $(LIBOPENCL_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	@echo "Creating egl/libGLES_mali.so symlink: $@"
+	@echo "Creating lib/libOpenCL.so symlink: $@"
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf libOpenCL.so.1 $@
+
+LIBOPENCL64_SYMLINK := $(TARGET_OUT_VENDOR)/lib64/libOpenCL.so
+$(LIBOPENCL64_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating lib64/libOpenCL.so symlink: $@"
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf libOpenCL.so.1 $@
+
+LIBOPENCL1_SYMLINK := $(TARGET_OUT_VENDOR)/lib/libOpenCL.so.1
+$(LIBOPENCL1_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating lib/libOpenCL.so.1 symlink: $@"
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf libOpenCL.so.1.1 $@
+
+LIBOPENCL641_SYMLINK := $(TARGET_OUT_VENDOR)/lib64/libOpenCL.so.1
+$(LIBOPENCL641_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating lib64/libOpenCL.so.1 symlink: $@"
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf libOpenCL.so.1.1 $@
+
+LIBOPENCL11_SYMLINK := $(TARGET_OUT_VENDOR)/lib/libOpenCL.so.1.1
+$(LIBOPENCL11_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating lib/libOpenCL.so.1.1 symlink: $@"
 	@mkdir -p $(dir $@)
 	$(hide) ln -sf $(LIBGLES_MALI_LIBRARY) $@
 
+LIBOPENCL6411_SYMLINK := $(TARGET_OUT_VENDOR)/lib64/libOpenCL.so.1.1
+$(LIBOPENCL6411_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating lib64/libOpenCL.so.1.1 symlink: $@"
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf $(LIBGLES_MALI64_LIBRARY) $@
+
 ALL_DEFAULT_INSTALLED_MODULES += \
-	$(LIBOPENCL_SYMLINK)
+	$(LIBOPENCL_SYMLINK) \
+	$(LIBOPENCL64_SYMLINK) \
+	$(LIBOPENCL1_SYMLINK) \
+	$(LIBOPENCL641_SYMLINK) \
+	$(LIBOPENCL11_SYMLINK) \
+	$(LIBOPENCL6411_SYMLINK)
