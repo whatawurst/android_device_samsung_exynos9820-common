257d8b3facb83fd50950182c902659e3fd243c37
Tim Zimmermann <tim@linux4.de>
2020-04-11 07:12:22 +0200

    platform: fix PRODUCT_COPY_FILES

--- platform/config.mk
+++ platform/config.mk
@@ -15,8 +15,8 @@ PRODUCT_COPY_FILES += \
 
 ### GPS
 PRODUCT_COPY_FILES += \
-    $(PLATFORM_PATH)/config/gps/gps_psds.conf:$(TARGET_COPY_OUT)/etc/gps/gps_psds.conf
+    $(PLATFORM_PATH)/config/gps/gps_psds.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/gps/gps_psds.conf
 
 ### KEYLAYOUT
 PRODUCT_COPY_FILES += \
-    $(PLATFORM_PATH)/config/keylayout/gpio_keys.kl:$(TARGET_COPY_OUT)/usr/keylayout/gpio_keys.kl
+    $(PLATFORM_PATH)/config/keylayout/gpio_keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio_keys.kl
