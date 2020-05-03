b3f9b42f7e0a373c8fb9e25673daf6ff2a6bb9e9
Tim Zimmermann <tim@linux4.de>
2020-05-03 11:13:02 +0200

    platform: use lineage charger
    Animation from: https://github.com/PixelExperience/vendor_pixelstyle/tree/ten/charger

--- platform/config.mk
+++ platform/config.mk
@@ -13,6 +13,13 @@ PRODUCT_COPY_FILES += \
     $(PLATFORM_PATH)/config/audio/audio_policy_configuration_sec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(PLATFORM_PATH)/config/audio/audio_policy_volumes_sec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml
 
+### CHARGER
+PRODUCT_COPY_FILES += \
+    $(PLATFORM_PATH)/config/charger/animation.txt:$(TARGET_COPY_OUT_PRODUCT)/etc/res/values/charger/animation.txt \
+    $(PLATFORM_PATH)/config/charger/battery_scale.png:$(TARGET_COPY_OUT_PRODUCT)/etc/res/images/charger/battery_scale.png \
+    $(PLATFORM_PATH)/config/charger/battery_fail.png:$(TARGET_COPY_OUT_PRODUCT)/etc/res/images/charger/battery_fail.png \
+    $(PLATFORM_PATH)/config/charger/main_font.png:$(TARGET_COPY_OUT_PRODUCT)/etc/res/images/charger/main_font.png
+
 ### GPS
 PRODUCT_COPY_FILES += \
     $(PLATFORM_PATH)/config/gps/gps_psds.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/gps/gps_psds.conf
