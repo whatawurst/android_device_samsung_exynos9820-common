cd2dbf463ce0873ba493b5b6b6dbe2aed6dcf4c1
Andreas Schneider <asn@cryptomilk.org>
2020-07-01 08:26:42 +0200

    config: Add powerhint.json for power-libperfmgr
    Change-Id: I31e5b0cd2fb24dcfbc25fe7b33a9a6a2b151ef2a

--- platform/config.mk
+++ platform/config.mk
@@ -32,3 +32,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(PLATFORM_PATH)/config/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(PLATFORM_PATH)/config/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
+
+### POWER
+PRODUCT_COPY_FILES += \
+    $(PLATFORM_PATH)/config/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
