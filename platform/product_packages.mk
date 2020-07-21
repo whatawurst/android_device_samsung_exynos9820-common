9980ca209ffbb8edecb5b79955c1ab4c33065c3b
Andreas Schneider <asn@cryptomilk.org>
2020-07-21 16:33:24 +0200

    platform: Build android.hardware.thermal@2.0-service.samsung
    https://review.lineageos.org/q/topic:samsung-thermal
    
    Change-Id: I47d610613af70a79d12ce0268edcc4a411bb61d0

--- platform/product_packages.mk
+++ platform/product_packages.mk
@@ -161,8 +161,7 @@ PRODUCT_PACKAGES += \
 
 ### THERMAL
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
+    android.hardware.thermal@2.0-service.samsung
 
 ### USB
 PRODUCT_PACKAGES += \
