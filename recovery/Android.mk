55f52e9d41de3a6b8f0963fdc3258bf7a33a2080
Erfan Abdi <erfangplus@gmail.com>
2020-09-23 12:22:46 +0330

    recovery: Update init script and fix USB

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -2,12 +2,15 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := init.recovery.exynos9820
+ifeq ($(WITH_TWRP), true)
+LOCAL_SRC_FILES := init.recovery.twrp.exynos9820.rc
+else
 LOCAL_SRC_FILES := init.recovery.exynos9820.rc
+endif # WITH_TWRP
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_STEM := init.recovery.$(TARGET_DEVICE)
-LOCAL_MODULE_SUFFIX := .rc
+LOCAL_MODULE_STEM := init.recovery.exynos9820.rc
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 ifeq ($(TW_EXCLUDE_DEFAULT_USB_INIT), true)
