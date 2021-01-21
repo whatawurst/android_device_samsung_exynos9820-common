fd9e8d33686363c5425268377c2babb9caa64092
Andreas Schneider <asn@cryptomilk.org>
2021-01-21 09:49:18 +0100

    recovery: Remove obsolete eng module tag

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -14,7 +14,7 @@ ifeq ($(TW_EXCLUDE_DEFAULT_USB_INIT), true)
 include $(CLEAR_VARS)
 LOCAL_MODULE := init.recovery.usb.rc
 LOCAL_SRC_FILES := init.recovery.usb.rc
-LOCAL_MODULE_TAGS := optional eng
+LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
 LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
